007264e9c78d4e57ecf1cebd1c253939dd55fc97
Ali Skinner <awskinner26@gmail.com>
2017-04-05 00:09:44 +0100

    added test for GET /entries

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Entry} = require('./../models/entry');
 
+const entries =[{
+  text: 'first test entry'
+}, {
+  text: 'second test entry'
+}]
+
 beforeEach((done) => {
-  Entry.remove({}).then(() => done());
+  Entry.remove({}).then(() => {
+    return Entry.insertMany(entries);
+  }).then(() => done());
 });
 
 describe('POST /entries', () => {
@@ -24,7 +32,7 @@ describe('POST /entries', () => {
             return done(err);
           }
 
-          Entry.find().then((entries) => {
+          Entry.find({text}).then((entries) => {
             expect(entries.length).toBe(1);
             expect(entries[0].text).toBe(text);
             done();
@@ -40,17 +48,27 @@ describe('POST /entries', () => {
       .send({})
       .expect(400)
       .end((err, res) => {
-        
         if (err) {
           return done(err);
         }
 
         Entry.find().then((entries) => {
-          expect(entries.length).toBe(0);
+          expect(entries.length).toBe(2);
           done()
         }).catch((e) => done(e));
 
       });
   });
+});
 
+describe('GET /entries', () => {
+  it('should get all entries', (done) => {
+      request(app)
+        .get('/entries')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.entries.length).toBe(2)
+        })
+        .end(done);
+  });
 });
